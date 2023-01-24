ca7001ded26c6c4c81d06e4d00d48548c207c812
luiz567 <martins.567@hotmail.com>
2023-01-24 15:41:31 +0000

    Alteração no script, agora os logs são gerados na pasta com meu nome dentro do diretorio montado no nfs

--- scripts/scriptCheckApache.sh
+++ scripts/scriptCheckApache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 StatusDoServico=$(service httpd status)
         if [[ $StatusDoServico == *"active (running)"* ]];
-        then echo "O servico HTTPD esta ativo $(date +%d/%m/%y) as $(date +%H:%M:%S)-APACHE esta ONLINE" | tee -a /scripts/log_online.txt;
-        else echo "O servico HTTPD esta desativo$(date +%d/%m/%y) as $(date +%H:%M:%S)-APACHE esta OFFLINE" | tee -a /scripts/log_offline.txt;
+        then echo "O servico HTTPD esta ativo $(date +%d/%m/%y) as $(date +%H:%M:%S)-APACHE esta ONLINE" | tee -a /nfs/LuizGustavo/log_online.txt;
+        else echo "O servico HTTPD esta desativo$(date +%d/%m/%y) as $(date +%H:%M:%S)-APACHE esta OFFLINE" | tee -a /nfs/LuizGustavo/log_offline.txt;
         fi
